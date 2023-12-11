1ed195c2895a5e8bc191aafd328cfb1d56cdcaa8
Maple <wang-jiaxun@outlook.com>
2023-12-11 18:17:59 +0800

    add VS2019 compatibility

--- defines.py
+++ defines.py
@@ -3,6 +3,9 @@ from pathlib import Path
 CL_DIR = "C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/"
 CL_DIR = Path(CL_DIR).resolve()
 
+CL_DIR_2019_64 = "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64"
+CL_DIR_2019_64 = Path(CL_DIR_2019_64).resolve()
+
 
 CL_ORIGIN = CL_DIR.joinpath("cl2.exe")
 CURRENT = Path(__file__).resolve().parent
--- init_shim_config.py
+++ init_shim_config.py
@@ -1,11 +1,20 @@
-from defines import CURRENT, CL_DIR
+from defines import *
+import argparse
 
 wrapper = CURRENT.joinpath("cl_wrapper.py")
 wrapper_path = str(wrapper)
-shim_file = CL_DIR.joinpath("cl.shim")
 
 
-def write_shim_config():
+def get_cl_dir(vs_version: str):
+    if vs_version == "2008":
+        return CL_DIR
+    elif vs_version == "2019_64":
+        return CL_DIR_2019_64
+
+
+def write_shim_config(vs_version: str):
+    cl_dir = get_cl_dir(vs_version=vs_version)
+    shim_file = cl_dir.joinpath("cl.shim")
     shim_file.write_text(
         f"""path = python.exe
 args = {wrapper_path}"""
@@ -13,4 +22,7 @@ args = {wrapper_path}"""
 
 
 if __name__ == "__main__":
-    write_shim_config()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--vs-version", "-V", required=True)
+    args = parser.parse_args()
+    write_shim_config(args.vs_version)
